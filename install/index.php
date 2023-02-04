d9333b62e18fb61e47d97348bea220567c3f3670
Yasha <yasha@iagent.ru>
2023-02-04 09:14:19 +0300

    Update index.php

--- install/index.php
+++ install/index.php
@@ -1,10 +1,6 @@
 <?
 $MODULE_ID = array_slice(explode("/", __DIR__), -2, 1)[0];
 
-$path = __DIR__ . "/install.php";
-
-echo '<pre>$path in ind : '; print_r($path); echo'</pre>';
-
-// if ( !class_exists($MODULE_ID) )
+if ( !class_exists($MODULE_ID) )
 	include(__DIR__ . "/install.php");
 ?>
