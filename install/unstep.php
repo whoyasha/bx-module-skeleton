598e145089c6da4dcd1b74344ff84dd090554c95
Yasha <yasha@iagent.ru>
2023-02-04 10:10:09 +0300

    Update unstep.php

--- install/unstep.php
+++ install/unstep.php
@@ -1,4 +1,16 @@
 <?if(!check_bitrix_sessid()) return;
 $MODULE_ID = array_slice(explode("/", __DIR__), -2, 1)[0];
 echo CAdminMessage::ShowNote("Модуль \"" . $MODULE_ID . "\" успешно удален из системы");
+
+$dir = str_replace('install', '', __DIR__) . "log";
+removeDir($dir);
+
+function removeDir($dir) {
+	if ($objs = glob($dir . '/*')) {
+		foreach($objs as $obj) {
+			is_dir($obj) ? remove_dir($obj) : unlink($obj);
+		}
+	}
+	rmdir($dir);
+}
 ?>
