b071e27c8d6a6e42f03a817d7697805633d00336
Yasha Okoneshnikov <y@iagent.ru>
2023-02-05 14:09:39 +0300

    test

--- include.php
+++ include.php
@@ -6,10 +6,4 @@ if ( file_exists(__DIR__ . "/install/classes/helper.php") )
 $MODULE_ID = array_pop(explode("/", __DIR__));
 $init = initBxModule($MODULE_ID);
 
-
-
-
-
-echo '<pre>$init in ' . __FUNCTION__ . ' : '; print_r($init); echo'</pre>';
-
 ?>
