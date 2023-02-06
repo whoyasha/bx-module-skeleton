565e4cc34d38737b789f1c16ef07e7ce776df014
Yasha <yasha@iagent.ru>
2023-02-06 04:37:27 +0300

    Delete include.php

--- include.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$MODULE_ID = array_pop(explode("/", __DIR__));
-\Bitrix\Main\Loader::registerAutoLoadClasses($MODULE_ID, ['BxModule' => "config.php"]);
-
-$config = BxModule::config($MODULE_ID);
-
-?>
