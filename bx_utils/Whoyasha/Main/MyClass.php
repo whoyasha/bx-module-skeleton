7b4f5b4abcb8e5d80b64cd5ed1693babce5f4429
Yasha cOkoneshnikov <yasha@iagent.ru>
2023-02-06 04:52:31 +0300

    test

---
+++ bx_utils/Whoyasha/Main/MyClass.php
@@ -0,0 +1,8 @@
+<?php 
+namespace Whoyasha\Main;
+
+Class MyClass {
+
+}
+
+?>
--- install/version.php
+++ install/version.php
@@ -1,1 +1,1 @@
-<?php $arVersion = ["VERSION" => "1.0.0-beta", "VERSION_DATE" => "2023-02-06"]; ?>
+<?php $arVersion = ["VERSION" => "dev-master", "VERSION_DATE" => "2023-02-06"]; ?>
